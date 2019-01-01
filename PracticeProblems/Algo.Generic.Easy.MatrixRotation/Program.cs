1126fd27e28a492bda3050ad123f9855d9124eb2
Zeeshan Vohra <zeeshanv55@gmail.com>
2019-01-01 12:31:06 +0530

    Added matric rotation problems

---
+++ PracticeProblems/Algo.Generic.Easy.MatrixRotation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo.Generic.Easy.MatrixRotation
+{
+    class Program
+    {
+        static int[][] rotate(int[][] matrix)
+        {
+            var rotatedMatrix = new int[matrix[0].Length][];
+            for (var i = 0; i < matrix[0].Length; i++)
+            {
+                rotatedMatrix[i] = new int[matrix.Length];
+                for (var j = 0; j < matrix.Length; j++)
+                {
+                    rotatedMatrix[i][j] = matrix[matrix.Length - 1 - j][i];
+                }
+            }
+
+            return rotatedMatrix;
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"C:\Users\zeesvo\Desktop\input.txt");
+            var nm = textReader.ReadLine();
+            var n = Convert.ToInt32(nm.Split(' ')[0]);
+            var m = Convert.ToInt32(nm.Split(' ')[1]);
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Array.ConvertAll(textReader.ReadLine().Split(' '), x => Convert.ToInt32(x));
+            }
+
+            var rotatedMatrix = rotate(matrix);
+
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    Console.Write(rotatedMatrix[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticeProblems/Algo.Generic.Easy.MatrixRotationInPlace/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Algo.Generic.Easy.MatrixRotationInPlace
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static int[][] rotate(int[][] matrix)
+        {
+            for (var i = 0; i < matrix.Length/2; i++)
+            {
+                for (var j = i; j < matrix.Length - i - 1; j++)
+                {
+                    var t = matrix[i][j];
+                    matrix[i][j] = matrix[matrix.Length - 1 - j][i];
+                    matrix[matrix.Length - 1 - j][i] = matrix[matrix.Length - 1 - i][matrix.Length - 1 - j];
+                    matrix[matrix.Length - 1 - i][matrix.Length - 1 - j] = matrix[j][matrix.Length - 1 - i];
+                    matrix[j][matrix.Length - 1 - i] = t;
+                }
+            }
+
+            return matrix;
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"C:\Users\zeesvo\Desktop\input.txt");
+            var n = Convert.ToInt32(textReader.ReadLine());
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Array.ConvertAll(textReader.ReadLine().Split(' '), x => Convert.ToInt32(x));
+            }
+
+            var rotatedMatrix = rotate(matrix);
+
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    Console.Write(rotatedMatrix[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
