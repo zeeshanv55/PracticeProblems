94cf282b1193b8a348d2d83526abf97e3e6d9b63
Zeeshan Vohra <zeeshanv55@gmail.com>
2019-02-03 21:03:15 +0530

    Added basic heap implementation, dijkstra's min path algo and kosaraju's strongly connected components algo

---
+++ PracticeProblems/Ds.Graphs.Medium.DijkstraMinPath/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ds.Graphs.Medium.DijkstraMinPath
+{
+    class Program
+    {
+        const int N = 200;
+
+        static int DijkstraMinLength(int source, int destination, Dictionary<int, int>[] adjacencyArray)
+        {
+            var discoveredSet = new HashSet<int>();
+            var dijkstraScores = new int[N];
+
+            discoveredSet.Add(source);
+            dijkstraScores[source] = 0;
+
+            while (discoveredSet.Count < N)
+            {
+                var minLength = int.MaxValue;
+                var minEdgeHead = -1;
+
+                foreach (var s in discoveredSet)
+                {
+                    foreach (var e in adjacencyArray[s])
+                    {
+                        if (!discoveredSet.Contains(e.Key) && dijkstraScores[s] + e.Value < minLength)
+                        {
+                            minLength = dijkstraScores[s] + e.Value;
+                            minEdgeHead = e.Key;
+                        }
+                    }
+                }
+
+                dijkstraScores[minEdgeHead] = minLength;
+                discoveredSet.Add(minEdgeHead);
+            }
+
+            return dijkstraScores[destination];
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"input.txt");
+            var adjacencyArray = new Dictionary<int, int>[N];
+
+            while (!textReader.EndOfStream)
+            {
+                var line = textReader.ReadLine().Trim().Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var currentIndex = Convert.ToInt32(line[0]) - 1;
+                adjacencyArray[currentIndex] = new Dictionary<int, int>();
+
+                for (var i = 1; i < line.Length; i++)
+                {
+                    var edge = line[i].Trim().Split(',');
+                    if (adjacencyArray[currentIndex].ContainsKey(Convert.ToInt32(edge[0]) - 1))
+                    {
+                        if (Convert.ToInt32(edge[1]) < adjacencyArray[currentIndex][Convert.ToInt32(edge[0]) - 1])
+                        {
+                            adjacencyArray[currentIndex][Convert.ToInt32(edge[0]) - 1] = Convert.ToInt32(edge[1]);
+                        }
+                    }
+                    else
+                    {
+                        adjacencyArray[currentIndex].Add(Convert.ToInt32(edge[0]) - 1, Convert.ToInt32(edge[1]));
+                    }
+                }
+            }
+
+            Console.Write(DijkstraMinLength(0, 6, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 36, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 58, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 81, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 98, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 114, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 132, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 164, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 187, adjacencyArray) + ",");
+            Console.Write(DijkstraMinLength(0, 196, adjacencyArray));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticeProblems/Ds.Graphs.Medium.KosarajuScc/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ds.Graphs.Medium.KosarajuScc
+{
+    class Program
+    {
+        const int N = 875714;
+        
+        static int[] RunningTimes = new int[N];
+
+        static int[] Leaders = new int[N];
+
+        static int T = 0;
+
+        static int S;
+
+        static HashSet<int> ExploredSet = new HashSet<int>();
+
+        public static void DepthFirstSearch(List<int>[] adjacencyArray, int s)
+        {
+            var dfsStack = new Stack<int>();
+            dfsStack.Push(s);
+            ExploredSet.Add(s);
+            Leaders[S]++;
+
+            while (dfsStack.Any())
+            {
+                var v = dfsStack.Peek();
+
+                var foundNewVertices = false;
+                for (var i = 0; i < adjacencyArray[v].Count; i++)
+                {
+                    if (!ExploredSet.Contains(adjacencyArray[v][i]))
+                    {
+                        foundNewVertices = true;
+                        dfsStack.Push(adjacencyArray[v][i]);
+                        ExploredSet.Add(adjacencyArray[v][i]);
+                        Leaders[S]++;
+                    }
+                }
+
+                if (!foundNewVertices)
+                {
+                    dfsStack.Pop();
+                    RunningTimes[T] = v;
+                    T++;
+                }
+            }
+        }
+
+        public static List<int> GetSccs(List<int>[] adjacencyArray, List<int>[] reverseAdjacencyArray)
+        {
+            for (var i = N - 1; i >= 0; i--)
+            {
+                if (!ExploredSet.Contains(i))
+                {
+                    S = i;
+                    DepthFirstSearch(reverseAdjacencyArray, i);
+                }
+            }
+
+            ExploredSet = new HashSet<int>();
+            Leaders = new int[N];
+            T = 0;
+            
+            for (var i = N - 1; i >= 0; i--)
+            {
+                if (!ExploredSet.Contains(RunningTimes[i]))
+                {
+                    S = RunningTimes[i];
+                    DepthFirstSearch(adjacencyArray, RunningTimes[i]);
+                }
+            }
+
+            var leaderList = Leaders.ToList();
+            leaderList.Sort();
+            return leaderList;
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"input.txt");
+            var adjacencyArray = new List<int>[N];
+            var reverseAdjacencyArray = new List<int>[N];
+
+            for (var i = 0; i < N; i++)
+            {
+                adjacencyArray[i] = new List<int>();
+                reverseAdjacencyArray[i] = new List<int>();
+            }
+
+            while (!textReader.EndOfStream)
+            {
+                var line = textReader.ReadLine().Trim().Split(' ');
+                adjacencyArray[Convert.ToInt32(line[0]) - 1].Add(Convert.ToInt32(line[1]) - 1);
+                reverseAdjacencyArray[Convert.ToInt32(line[1]) - 1].Add(Convert.ToInt32(line[0]) - 1);
+            }
+
+            var leaderList = GetSccs(adjacencyArray, reverseAdjacencyArray);
+
+            for (var i = N - 1; i >= N - 6; i--)
+            {
+                Console.WriteLine(leaderList[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
