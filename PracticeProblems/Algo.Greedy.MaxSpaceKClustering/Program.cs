0d00b6219d380a14153e11535c1a1d502dae2654
Zeeshan Vohra <zeeshanv55@gmail.com>
2019-02-09 17:50:57 +0530

    Added greedy clustering problems

---
+++ PracticeProblems/Algo.Greedy.MaxSpaceKClustering/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace Algo.Greedy.MaxSpaceKClustering
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class Program
+    {
+        static Random RandomGenerator { get; set; }
+
+        const int K = 4;
+
+        static int N { get; set; }
+
+        public static int Partition(int startIndex, int endIndex, Dictionary<Tuple<long, long>, double> edgeList, List<Tuple<long, long>> edgeKeys)
+        {
+            var randomMedian = RandomGenerator.Next(startIndex, endIndex + 1);
+
+            var t = edgeKeys[startIndex];
+            edgeKeys[startIndex] = edgeKeys[randomMedian];
+            edgeKeys[randomMedian] = t;
+
+            var i = startIndex + 1;
+            var j = startIndex + 1;
+
+            while (i <= endIndex && j <= endIndex)
+            {
+                if (edgeList[edgeKeys[j]] < edgeList[edgeKeys[startIndex]])
+                {
+                    t = edgeKeys[i];
+                    edgeKeys[i] = edgeKeys[j];
+                    edgeKeys[j] = t;
+
+                    i++;
+                }
+
+                j++;
+            }
+
+            t = edgeKeys[i - 1];
+            edgeKeys[i - 1] = edgeKeys[startIndex];
+            edgeKeys[startIndex] = t;
+
+            return i - 1;
+        }
+
+        static void QuickSortEdges(int startIndex, int endIndex, Dictionary<Tuple<long, long>, double> edgeList, List<Tuple<long, long>> edgeKeys)
+        {
+            if (endIndex <= startIndex)
+            {
+                return;
+            }
+
+            var partitionIndex = -1;
+            partitionIndex = Partition(startIndex, endIndex, edgeList, edgeKeys);
+
+            QuickSortEdges(startIndex, partitionIndex - 1, edgeList, edgeKeys);
+            QuickSortEdges(partitionIndex + 1, endIndex, edgeList, edgeKeys);
+        }
+
+        static double MaxSpaceKCluster(Dictionary<Tuple<long, long>, double> edgeList, List<Tuple<long, long>> edgeKeys, int n, int k)
+        {
+            var leaderLists = new Dictionary<long, List<long>>();
+            var leaderPointers = new long[n];
+            for (var i = 0; i < n; i++)
+            {
+                leaderPointers[i] = i;
+                leaderLists[i] = new List<long> { i };
+            }
+
+            RandomGenerator = new Random();
+            QuickSortEdges(0, edgeKeys.Count - 1, edgeList, edgeKeys);
+
+            while (leaderLists.Count > k)
+            {
+                var smallestEdge = edgeKeys[0];
+                var v1 = smallestEdge.Item1;
+                var v2 = smallestEdge.Item2;
+                
+                if (leaderPointers[v1] != leaderPointers[v2])
+                {
+                    if (leaderLists[leaderPointers[v1]].Count >= leaderLists[leaderPointers[v2]].Count)
+                    {
+                        var leaderToRemove = leaderPointers[v2];
+                        foreach (var v in leaderLists[leaderPointers[v2]])
+                        {
+                            leaderLists[leaderPointers[v1]].Add(v);
+                            leaderPointers[v] = leaderPointers[v1];
+                        }
+
+                        leaderLists.Remove(leaderToRemove);
+                    }
+                    else
+                    {
+                        var leaderToRemove = leaderPointers[v1];
+                        foreach (var v in leaderLists[leaderPointers[v1]])
+                        {
+                            leaderLists[leaderPointers[v2]].Add(v);
+                            leaderPointers[v] = leaderPointers[v2];
+                        }
+
+                        leaderLists.Remove(leaderToRemove);
+                    }
+                }
+
+                edgeKeys.RemoveAt(0);
+            }
+
+            while (leaderPointers[edgeKeys[0].Item1] == leaderPointers[edgeKeys[0].Item2])
+            {
+                edgeKeys.RemoveAt(0);
+            }
+
+            return edgeList[edgeKeys[0]];
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"input.txt");
+            N = Convert.ToInt32(textReader.ReadLine().Trim());
+            var edgeList = new Dictionary<Tuple<long, long>, double>();
+            var edgeKeys = new List<Tuple<long, long>>();
+
+            while (!textReader.EndOfStream)
+            {
+                var line = textReader.ReadLine().Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var i = Convert.ToInt64(line[0]) - 1;
+                var j = Convert.ToInt64(line[1]) - 1;
+                var e = Convert.ToInt64(line[2]);
+
+                var edgeKey = new Tuple<long, long>(Math.Min(i, j), Math.Max(i, j));
+                if (!edgeList.ContainsKey(edgeKey))
+                {
+                    edgeKeys.Add(edgeKey);
+                    edgeList.Add(edgeKey, e);
+                }
+                else
+                {
+                    if (e < edgeList[edgeKey])
+                    {
+                        edgeList[edgeKey] = e;
+                    }
+                }
+            }
+
+            Console.WriteLine(MaxSpaceKCluster(edgeList, edgeKeys, N, K));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticeProblems/Algo.Greedy.MaxSpaceKClusteringReverse/Program.cs
@@ -0,0 +1,189 @@
+﻿namespace Algo.Greedy.MaxSpaceKClusteringReverse
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    class Program
+    {
+        const int D = 3;
+
+        public static HashSet<int> DepthFirstSearch(List<int>[] adjacencyList, int s)
+        {
+            var exploredSet = new HashSet<int>();
+            var dfsStack = new Stack<int>();
+            dfsStack.Push(s);
+            exploredSet.Add(s);
+
+            while (dfsStack.Count > 0)
+            {
+                var t = dfsStack.Pop();
+                foreach (var v in adjacencyList[t])
+                {
+                    if (!exploredSet.Contains(v))
+                    {
+                        dfsStack.Push(v);
+                        exploredSet.Add(v);
+                    }
+                }
+            }
+
+            return exploredSet;
+        }
+
+        static string BitFlip(string s, int i)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            var resultBuilder = new StringBuilder();
+            for (var j = 0; j < s.Length; j++)
+            {
+                if (i == j)
+                {
+                    if (s[j] == '0')
+                    {
+                        resultBuilder.Append('1');
+                    }
+                    else
+                    {
+                        resultBuilder.Append('0');
+                    }
+                }
+                else
+                {
+                    resultBuilder.Append(s[j]);
+                }
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        static HashSet<string> GetPossibleStringsWithDistance(string s, int d)
+        {
+            var returnSet = new HashSet<string>();
+
+            if (d == 1)
+            {
+                for (var i = 0; i < s.Length; i++)
+                {
+                    returnSet.Add(BitFlip(s, i));
+                }
+            }
+            else
+            {
+                for (var i = 0; i < s.Length - 1; i++)
+                {
+                    var preStringBuilder = new StringBuilder();
+
+                    for (var j = 0; j < i; j++)
+                    {
+                        preStringBuilder.Append(s[j]);
+                    }
+
+                    if (s[i] == '0')
+                    {
+                        preStringBuilder.Append('1');
+                    }
+                    else
+                    {
+                        preStringBuilder.Append('0');
+                    }
+
+                    var preString = preStringBuilder.ToString();
+                    var postStrings = GetPossibleStringsWithDistance(s.Substring(i + 1), d - 1);
+
+                    foreach (var s2 in postStrings)
+                    {
+                        returnSet.Add($"{preString}{s2}");
+                    }
+                }
+            }
+
+            return returnSet;
+        }
+
+        static HashSet<string> GetPossibleStringsWithDistanceUpto(string s, int d)
+        {
+            var returnSet = new HashSet<string>();
+
+            for (var i = 1; i <= d; i++)
+            {
+                var partialSet = GetPossibleStringsWithDistance(s, i);
+                foreach (var s1 in partialSet)
+                {
+                    returnSet.Add(s1);
+                }
+            }
+
+            return returnSet;
+        }
+
+        static int MaxSpaceKClusterReverse(HashSet<string> inputSet)
+        {
+            var inputList = new List<string>();
+            foreach (var s in inputSet)
+            {
+                inputList.Add(s);
+            }
+
+            var inputDictionary = new Dictionary<string, int>();
+            var leaderLists = new List<int>[inputList.Count];
+
+            for (var i = 0; i < inputList.Count; i++)
+            {
+                leaderLists[i] = new List<int>();
+                inputDictionary.Add(inputList[i], i);
+            }
+
+            for (var i = 0; i < inputList.Count; i++)
+            {
+                var allPossibleNeighbours = GetPossibleStringsWithDistanceUpto(inputList[i], D - 1);
+                foreach (var p in allPossibleNeighbours)
+                {
+                    if (inputSet.Contains(p))
+                    {
+                        leaderLists[i].Add(inputDictionary[p]);
+                    }
+                }
+            }
+
+            var clusteredSets = new List<HashSet<string>>();
+            for (var i = 0; i < inputList.Count; i++)
+            {
+                if (inputSet.Contains(inputList[i]))
+                {
+                    var reachableSet = DepthFirstSearch(leaderLists, i);
+                    var clusteredSet = new HashSet<string>();
+                    foreach (var v in reachableSet)
+                    {
+                        inputSet.Remove(inputList[v]);
+                        clusteredSet.Add(inputList[v]);
+                    }
+
+                    clusteredSets.Add(clusteredSet);
+                }
+            }
+
+            return clusteredSets.Count;
+        }
+
+        static void Main(string[] args)
+        {
+            var textReader = new StreamReader(@"input.txt");
+            var line = textReader.ReadLine();
+            var inputSet = new HashSet<string>();
+
+            while (!textReader.EndOfStream)
+            {
+                inputSet.Add(textReader.ReadLine().Trim().Replace(" ", string.Empty));
+            }
+
+            Console.WriteLine(MaxSpaceKClusterReverse(inputSet));
+            Console.ReadKey();
+        }
+    }
+}
